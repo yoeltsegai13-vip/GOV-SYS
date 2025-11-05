2b858c43f76e622a88fc875c7cc023cc460dfeb6
yoeltsegai13-vip <yoeltsegai13@gmail.com>
2025-11-05 17:42:22 +0300

    COMMIT-1
    This GovernmentSystem class with a main method creates sub class of HealthDepartment, EducationDepartment, and DefenseDepartment, displays their details, and calculates budget usage for each department.

---
+++ GovernmentDepartment.java
@@ -0,0 +1,32 @@
+ abstract class GovernmentDepartment {
+    String departmentName;
+    double budget; // in million dollars
+
+    // Constructor to initialize common attributes
+    public GovernmentDepartment(String departmentName, double budget) {
+        this.departmentName = departmentName;
+        this.budget = budget;
+    }
+
+     public String getDepartmentName() {
+         return departmentName;
+     }
+<<<<<<< Updated upstream
+     
+=======
+
+>>>>>>> Stashed changes
+     public void showBudgetInfo() {
+         System.out.println(departmentName + " has a total budget of $" + budget + " million.");
+     }
+
+    // Abstract method to display department info
+    abstract void displayDetails();
+
+    // Concrete method to calculate used budget
+    public double calculateBudgetUsage(double percentage) {
+        return (budget * percentage) / 100;
+    }
+}
+
+
---
+++ GovernmentSystem.java
@@ -0,0 +1,26 @@
+public class GovernmentSystem {
+
+    public static void showDepartmentMessage(GovernmentDepartment department) {
+        System.out.println("Managing department: " + department.getName() + " efficiently.");
+    }
+
+    public static void main(String[] args) {
+        GovernmentDepartment health = new HealthDepartment("Health Ministry", 500.0, 120);
+        GovernmentDepartment education = new EducationDepartment("Education Ministry", 300.0, 1500);
+        GovernmentDepartment defense = new DefenseDepartment("Defense Ministry", 1000.0, 200000);
+
+        System.out.println("=== Government Department Details ===");
+
+        health.displayDetails();
+        System.out.println("Budget used (30%): $" + health.calculateBudgetUsage(30) + " million");
+        System.out.println("--------------------------------------");
+
+        education.displayDetails();
+        System.out.println("Budget used (25%): $" + education.calculateBudgetUsage(25) + " million");
+        System.out.println("--------------------------------------");
+
+        defense.displayDetails();
+        System.out.println("Budget used (40%): $" + defense.calculateBudgetUsage(40) + " million");
+        System.out.println("--------------------------------------");
+    }
+}
